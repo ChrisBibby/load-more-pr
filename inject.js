97decbb45fee7c105ebe268d2d412d25b8d28cd3
Chris Bibby <3396070+ChrisBibby@users.noreply.github.com>
2023-03-05 12:13:54 +0000

    feat: prevent button from being spammed

--- inject.js
+++ inject.js
@@ -1,24 +1,28 @@
 var lmprBtn = document.getElementById("lmpr-btn");
 if (lmprBtn === null) {
   const diffButton = document.createElement("div");
-  diffButton.onclick = () => {
-
-  [...document.querySelectorAll("button.load-diff-button")]
-    .filter( y => y.textContent
-      .includes("Load diff"))
-      .slice(0, 50)
-      .forEach((btn) => { 
-        setTimeout(() => btn.click(), 100)
-      });
-    
-    diffButton.setAttribute("aria-disabled", "true");
-    setTimeout(() => diffButton.setAttribute("aria-disabled", "false"), 5000);
-  };
-
   diffButton.setAttribute("class", "btn btn-sm");
   diffButton.setAttribute("id", "lmpr-btn");
   diffButton.setAttribute("style", "margin-left:10px; float:right;");
-  diffButton.textContent = "Load Diffs";
+  diffButton.textContent = "Load Next Diffs";
+
+  diffButton.onclick = () => {
+    if (diffButton.getAttribute("aria-disabled") != "true") {
+      diffButton.setAttribute("aria-disabled", "true");
+
+      [...document.querySelectorAll("button.load-diff-button")]
+        .filter(y => y.textContent
+          .includes("Load diff"))
+        .slice(0, 50)
+        .forEach((btn) => {
+          setTimeout(() => btn.click(), 100)
+        });
+
+      setTimeout(() => {
+        diffButton.setAttribute("aria-disabled", "false")
+      }, 5000);
+    }
+  };
 
   const prReviewTools = document.getElementsByClassName("pr-review-tools")[0];
   if (prReviewTools != undefined) {
